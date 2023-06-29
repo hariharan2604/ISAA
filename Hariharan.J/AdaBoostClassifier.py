64629ccd0f6436b5182026f87ebf14289ba9a888
Hariharan <hariharanjayachandran04@gmail.com>
2023-06-29 23:19:19 +0530

    Final draft

---
+++ Hariharan.J/AdaBoostClassifier.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+# Load the dataset
+data = pd.read_csv('../datasets/Microsoft Stocks.csv')
+
+# Extract features and labels
+X = data[['Open', 'High', 'Low', 'Volume']].values
+y = data['Price'].values
+
+# Define the price movement (1 if increase, 0 if decrease)
+y_movement = np.where(y[1:] > y[:-1], 1, 0)
+
+# Normalize features
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
+# Apply PCA
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_pca[:-1], y_movement, test_size=0.2, random_state=42)
+
+# Build the AdaBoostClassifier model
+model = AdaBoostClassifier()
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Evaluate the model
+y_pred = model.predict(X_test)
+
+# Calculate evaluation metrics
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
+print("Confusion Matrix:")
+print(conf_matrix)
---
+++ Hariharan.J/Convolutional Neural Networks (CNN).py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+# Load the dataset
+data = pd.read_csv('../datasets/Microsoft Stocks.csv')
+
+# Extract features and labels
+X = data[['Open', 'High', 'Low', 'Volume']].values
+y = data['Price'].values
+
+# Define the price movement (1 if increase, 0 if decrease)
+y_movement = np.where(y[1:] > y[:-1], 1, 0)
+
+# Normalize features
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X[:-1], y_movement, test_size=0.2, random_state=42)
+
+# Reshape the input data for CNN (assuming single-channel data)
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+
+# Build the CNN model
+model = Sequential()
+model.add(Conv1D(filters=32, kernel_size=3, activation='relu', input_shape=(X_train.shape[1], 1)))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=10, batch_size=32, verbose=1)
+
+# Evaluate the model
+y_pred = model.predict(X_test)
+y_pred_class = np.where(y_pred > 0.5, 1, 0)
+
+# Calculate evaluation metrics
+accuracy = accuracy_score(y_test, y_pred_class)
+precision = precision_score(y_test, y_pred_class)
+recall = recall_score(y_test, y_pred_class)
+f1 = f1_score(y_test, y_pred_class)
+conf_matrix = confusion_matrix(y_test, y_pred_class)
+
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
+print("Confusion Matrix:")
+print(conf_matrix)
